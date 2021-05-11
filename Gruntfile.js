e9169689eae29630b963c760cf4dca85f8dd92d5
riccio <riccio82@gmail.com>
2021-05-11 15:09:03 +0200

    update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,71 +1,59 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  var sass = require('node-sass')
 
-    var es2015Preset = require('babel-preset-env');
-    var reactPreset = require('babel-preset-react');
-    var sass = require('node-sass');
-
-    grunt.initConfig( {
-        browserify: {
-            core: {
-                options: {
-                    transform: [
-                        [ 'babelify', { presets: [ es2015Preset, reactPreset ] } ]
-                    ],
-                    browserifyOptions: {
-                        paths: [ __dirname + '/node_modules' ]
-                    }
-                },
-                src: [
-                    'static/src/js/cat_source/airbnb-core.js',
-                    'static/src/js/cat_source/airbnb-core.*.js'
-                ],
-                dest:  'static/build/airbnb-core-build.js'
-            },
+  grunt.initConfig({
+    browserify: {
+      core: {
+        options: {
+          transform: [
+            [
+              'babelify',
+              {presets: ['@babel/preset-react', ['@babel/preset-env']]},
+            ],
+          ],
+          browserifyOptions: {
+            paths: [__dirname + '/node_modules'],
+          },
         },
-        sass: {
-            dist: {
-                options: {
-                    implementation: sass,
-                    sourceMap: false,
-                    includePaths: ['static/src/css/sass/']
-                },
-                src: [
-                    'static/src/css/sass/airbnb-core.scss'
-                ],
-                dest: 'static/build/airbnb-build.css'
-            }
+        src: [
+          'static/src/js/cat_source/airbnb-core.js',
+          'static/src/js/cat_source/airbnb-core.*.js',
+        ],
+        dest: 'static/build/airbnb-core-build.js',
+      },
+    },
+    sass: {
+      dist: {
+        options: {
+          implementation: sass,
+          sourceMap: false,
+          includePaths: ['static/src/css/sass/'],
         },
-        replace: {
-            css: {
-                src: [
-                    'static/build/*'
-                ],
-                dest: 'static/build/',
-                replacements: [
-                    {
-                        from: 'url(../img',
-                        to: 'url(../src/css/img'
-                    }
-                ]
-            }
-        }
+        src: ['static/src/css/sass/airbnb-core.scss'],
+        dest: 'static/build/airbnb-build.css',
+      },
+    },
+    replace: {
+      css: {
+        src: ['static/build/*'],
+        dest: 'static/build/',
+        replacements: [
+          {
+            from: 'url(../img',
+            to: 'url(../src/css/img',
+          },
+        ],
+      },
+    },
+  })
 
-    });
+  grunt.loadNpmTasks('grunt-browserify')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-sass')
+  grunt.loadNpmTasks('grunt-text-replace')
 
-    grunt.loadNpmTasks('grunt-browserify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-text-replace');
+  // Define your tasks here
+  grunt.registerTask('default', ['bundle:js'])
 
-    // Define your tasks here
-    grunt.registerTask('default', ['bundle:js']);
-
-    grunt.registerTask('bundle:js', [
-        'browserify:core',
-        'sass',
-        'replace'
-    ]);
-
-
-
-};
+  grunt.registerTask('bundle:js', ['browserify:core', 'sass', 'replace'])
+}
