4bac9d8ae5912e4bb2a501142c91944344dd8463
Federico Ricciuti <riccio82@gmail.com>
2023-06-14 12:33:26 +0200

    Remove Deliver button (#86)

--- static/src/js/cat_source/airbnb-core.extension.js
+++ static/src/js/cat_source/airbnb-core.extension.js
@@ -1,167 +1,12 @@
 import Cookies from 'js-cookie'
 import _ from 'lodash'
-import {segmentDelivery} from './api/segmentDelivery'
 
 // Override characters size mapping
 TextUtils.charsSizeMapping = {
   default: (value) => TextUtils.getDefaultCharsSize(value),
 }
 
-const SegmentDeliveryModal =
-  require('./components/modals/SegmentDeliveryModal').default
 ;(function () {
-  const deliveryObj = {
-    showDelivery: null,
-    onTool: null,
-    signedJWT: null,
-  }
-
-  const _setDeliveryOrNull = function () {
-    if (config.pluggable && !_.isUndefined(config.pluggable.airbnb_ontool)) {
-      sessionStorage.setItem(config.id_job + ':showDelivery', '1')
-      sessionStorage.setItem(
-        config.id_job + ':deliveryEnabled',
-        config.pluggable.airbnb_ontool,
-      )
-    }
-    if (
-      config.pluggable &&
-      !_.isUndefined(config.pluggable.airbnb_auth_token)
-    ) {
-      sessionStorage.setItem(
-        config.id_job + ':airbnb_auth_token',
-        config.pluggable.airbnb_auth_token,
-      )
-    }
-    if (
-      config.pluggable &&
-      !_.isUndefined(config.pluggable.delivery_available)
-    ) {
-      sessionStorage.setItem(
-        config.id_job + ':delivery_available',
-        config.pluggable.delivery_available,
-      )
-    }
-    deliveryObj.onTool = !!parseInt(
-      sessionStorage.getItem(config.id_job + ':deliveryEnabled'),
-    )
-    deliveryObj.showDelivery = !!parseInt(
-      sessionStorage.getItem(config.id_job + ':showDelivery'),
-    )
-    deliveryObj.signedJWT = sessionStorage.getItem(
-      config.id_job + ':airbnb_auth_token',
-    )
-    deliveryObj.jobDelivarable =
-      sessionStorage.getItem(config.id_job + ':delivery_available') === 'true'
-  }
-
-  const setErrorNotification = function (title, message) {
-    _setNotification(title, message, 'error')
-  }
-
-  const setSuccessNotification = function (title, message) {
-    _setNotification(title, message, 'success')
-  }
-
-  const _setNotification = function (title, message, type) {
-    CatToolActions.addNotification({
-      title: title,
-      text: message,
-      type: type,
-      position: 'bl',
-      allowHtml: true,
-      autoDismiss: true,
-    })
-  }
-
-  _setDeliveryOrNull()
-  if (
-    deliveryObj.jobDelivarable &&
-    deliveryObj.showDelivery &&
-    deliveryObj.onTool
-  ) {
-    $(document).on('setTranslation:success', function (e, data) {
-      let segment = data.segment
-      $('.buttons .deliver', UI.getSegmentById(segment.sid)).removeClass(
-        'disabled',
-      )
-      sessionStorage.setItem('segToDeliver' + segment.sid, 1)
-      UI.blockButtons = false
-    })
-
-    $(document).on('click', 'section .buttons .deliver', function (e) {
-      e.preventDefault()
-      e.stopPropagation()
-      if ($(e.target).hasClass('disabled')) return
-      const segment = $(e.target).closest('section')
-      const idSeg = UI.getSegmentId(segment)
-      $('.buttons .deliver', segment).addClass('disabled')
-      sessionStorage.setItem('segToDeliver' + idSeg, 0)
-
-      if (deliveryObj.signedJWT === null) {
-        setErrorNotification(
-          'Segment not delivered',
-          'Session expired. Please Login again into TranslationOS.',
-        )
-        return
-      }
-
-      segmentDelivery(idSeg, deliveryObj.signedJWT)
-        .then((response) => {
-          setSuccessNotification(
-            'Segment delivered',
-            'Segment ' + idSeg + ' was delivered.',
-          )
-          if (window.opener) {
-            window.opener.postMessage(
-              {
-                name: 'segmentTranslationChange',
-                idSegment: toString(idSeg),
-                translation: response.translation,
-              },
-              '*',
-            )
-          }
-        })
-        .catch((e) => {
-          switch (e.status) {
-            case 401:
-              setErrorNotification(
-                'Segment not delivered',
-                'Session expired.<br/>Please Login again into TranslationOS.',
-              )
-              break
-            case 503:
-              setErrorNotification(
-                'Segment not delivered',
-                'We experienced an issue, please try again in 5 minutes, if the error occurs again, please, contact <a href="mailto:support@matecat.com">support@matecat.com</a>.',
-              )
-              break
-            default:
-              setErrorNotification(
-                'Segment not delivered',
-                'Oops we got an Error. Please, contact <a href="mailto:support@matecat.com">support@matecat.com</a>.',
-              )
-              break
-          }
-        })
-      return false
-    })
-  } else if (deliveryObj.showDelivery && !deliveryObj.onTool) {
-    $(document).on('click', 'section .buttons .deliver', function (e) {
-      e.preventDefault()
-      e.stopPropagation()
-      UI.openSegmentDeliveryModal()
-    })
-    $(document).on('files:appended', function () {
-      let cookie = Cookies.get('segment_delivery_modal_hide')
-      if (cookie !== '1') {
-        UI.openSegmentDeliveryModal()
-      }
-    })
-  } else if (deliveryObj.showDelivery && !deliveryObj.jobDelivarable) {
-    setTimeout(() => UI.openNotDeliverableJob(), 500)
-  }
 
   var originalRegisterFooterTabs = UI.registerFooterTabs
   var originalgoToNextSegment = UI.gotoNextSegment
@@ -171,34 +16,7 @@ const SegmentDeliveryModal =
     return false
   }
   $.extend(UI, {
-    openSegmentDeliveryModal: function () {
-      const props = {
-        modalName: 'segmentDeliveryModal',
-        text:
-          'This is an off-tool project. The segment delivery feature is disabled in Matecat. ' +
-          'You will have to deliver the whole project following the off-tool delivery procedure',
-      }
-      ModalsActions.showModalComponent(
-        SegmentDeliveryModal,
-        props,
-        'Segment delivery',
-      )
-    },
-    openNotDeliverableJob: function () {
-      const props = {
-        modalName: 'segmentDeliveryModal',
-        text:
-          'Due to an upgrade of the integration, phrase included in jobs older than 2020-08-25 cannot be delivered.' +
-          '</br>To fix and deliver a phrase, you can either:' +
-          '<ul><li>Initiate a Manual Fix request from Dragoman (preferred).</li>' +
-          '<li>Try to find the phrase in a more recent MateCat job and deliver from there.</li></ul>',
-      }
-      ModalsActions.showModalComponent(
-        SegmentDeliveryModal,
-        props,
-        'Segment delivery',
-      )
-    },
+
     registerFooterTabs: function () {
       originalRegisterFooterTabs.apply(this)
       SegmentActions.registerTab('messages', true, true)
@@ -315,19 +133,7 @@ const SegmentDeliveryModal =
         originalCreateButtons.apply(this, arguments)
       }
     },
-    inputEditAreaEventHandler: function () {
-      if (deliveryObj.onTool) {
-        $('.buttons .deliver', UI.currentSegment).addClass('disabled')
-      }
-      originalInputEditAreaEventHandler.apply(this, arguments)
-    },
-    gotoNextSegment: function () {
-      if (deliveryObj.onTool) {
-        return false
-      } else {
-        originalgoToNextSegment.apply(this, arguments)
-      }
-    },
+
   })
   function overrideTabMessages(SegmentTabMessages) {
     SegmentTabMessages.prototype.getNotes = function () {
@@ -689,119 +495,8 @@ const SegmentDeliveryModal =
     }
   }
 
-  function overrideGetTranslateButtons(SegmentButtons) {
-    var originalGetTranslateButton = SegmentButtons.prototype.getTranslateButton
-    SegmentButtons.prototype.getTranslateButton = function () {
-      const classDisable = this.props.disabled ? 'disabled' : ''
-      let deliveryButton
-      if (deliveryObj && deliveryObj.showDelivery) {
-        const deliveryDisabled =
-          sessionStorage.getItem('segToDeliver' + this.currentSegmentId) === '1'
-            ? ''
-            : 'disabled'
-        deliveryButton = (
-          <li>
-            <a
-              draggable="false"
-              id={'segment-' + this.currentSegmentId + '-button-deliver'}
-              data-segmentid={this.currentSegmentId}
-              href="#"
-              className={'deliver ' + deliveryDisabled}
-            >
-              DELIVER
-            </a>
-          </li>
-        )
-        return (
-          <React.Fragment>
-            <li>
-              <a
-                id={'segment-' + this.props.segment.sid + '-button-translated'}
-                onClick={(e) => this.clickOnTranslatedButton(e, false)}
-                data-segmentid={'segment-' + this.props.segment.sid}
-                className={'translated ' + classDisable}
-              >
-                {' '}
-                {config.status_labels.TRANSLATED}{' '}
-              </a>
-              <p>{UI.isMac ? 'CMD' : 'CTRL'} ENTER</p>
-            </li>
-            {deliveryButton}
-          </React.Fragment>
-        )
-      }
-      return originalGetTranslateButton.apply(this, arguments)
-    }
-  }
-
-  function overrideGetReviseButtons(SegmentButtons) {
-    var originalGetReviewButton = SegmentButtons.prototype.getReviewButton
-    SegmentButtons.prototype.getReviewButton = function () {
-      const classDisable = this.props.disabled ? 'disabled' : ''
-      const className = ReviewExtended.enabled()
-        ? 'revise-button-' + ReviewExtended.number
-        : ''
-
-      let deliveryButton
-      if (deliveryObj && deliveryObj.showDelivery) {
-        const deliveryDisabled =
-          sessionStorage.getItem('segToDeliver' + this.currentSegmentId) === '1'
-            ? ''
-            : 'disabled'
-        deliveryButton = (
-          <li>
-            <a
-              draggable="false"
-              id={'segment-' + this.currentSegmentId + '-button-deliver'}
-              data-segmentid={this.currentSegmentId}
-              href="#"
-              className={'deliver ' + deliveryDisabled}
-            >
-              DELIVER
-            </a>
-          </li>
-        )
-        return (
-          <React.Fragment>
-            <li>
-              <a
-                id={'segment-' + this.props.segment.sid + '-button-translated'}
-                onClick={(e) => this.clickOnApprovedButton(e, false)}
-                data-segmentid={'segment-' + this.props.segment.sid}
-                className={'approved ' + classDisable + ' ' + className}
-              >
-                {' '}
-                {config.status_labels.APPROVED}{' '}
-              </a>
-              <p>{UI.isMac ? 'CMD' : 'CTRL'} ENTER</p>
-            </li>
-            {deliveryButton}
-          </React.Fragment>
-        )
-      }
-      return originalGetReviewButton.apply(this, arguments)
-    }
-  }
-
-  function overrideGetContributionsSuccess(SegmentActions) {
-    var originalFunction = SegmentActions.getContributionsSuccess
-    SegmentActions.getContributionsSuccess = function (data, sid) {
-      if (config.source_code === 'en-US' && config.target_code === 'ja-JP') {
-        data.matches = _.filter(data.matches, function (match) {
-          return match.match !== 'MT'
-        })
-
-        return originalFunction.apply(this, [data, sid])
-      } else {
-        return originalFunction.apply(this, arguments)
-      }
-    }
-  }
-
   overrideTabMessages(SegmentTabMessages)
   overrideSetDefaultTabOpen(SegmentFooter)
-  overrideGetTranslateButtons(SegmentButtons)
-  overrideGetReviseButtons(SegmentButtons)
   ovverrideSegmentUtilFn(SegmentUtils)
 
   //Delete MT matches for japanese target and en-Us source
