51aad832b0644e89a17d8d9e8fc0c4eadfc4e8af
riccio82 <riccio82@gmail.com>
2025-07-07 12:34:52 +0200

    Update notes

--- static/src/cattool/airbnb-core.extension.js
+++ static/src/cattool/airbnb-core.extension.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {isUndefined, forOwn} from 'lodash'
-import TextUtils from '../../../../../public/js/cat_source/es6/utils/textUtils'
 import SegmentActions from '../../../../../public/js/cat_source/es6/actions/SegmentActions'
 import SegmentStore from '../../../../../public/js/cat_source/es6/stores/SegmentStore'
 import SegmentFooterTabMessages from '../../../../../public/js/cat_source/es6/components/segments/SegmentFooterTabMessages'
@@ -97,41 +96,10 @@ const init = () => {
       let notesHtml = []
       let self = this
       if (this.props.notes) {
-        const {metadata} = this.props
         this.props.notes.forEach((item, index) => {
           if (item.note && item.note !== '') {
             if (item.note.indexOf('¶') === -1) {
-              // jsont2
-              if (
-                metadata &&
-                metadata.find(({meta_key}) => meta_key === 'id_content') &&
-                metadata.find(({meta_key}) => meta_key === 'id_request')
-              ) {
-                let note = item.note
-                note = TextUtils.replaceUrl(note.replace(/[ ]*\n/g, '<br>\n'))
-                const html = (
-                  <div className="note" key={'note-' + index}>
-                    <span className="note-label">Note: </span>
-                    <span dangerouslySetInnerHTML={self.allowHTML(note)} />
-                  </div>
-                )
-                notesHtml.push(html)
-              } else {
-                let split = item.note.split(':')
-                if (split.length > 1) {
-                  let note = item.note.replace(split[0] + ':', '')
-                  note = TextUtils.replaceUrl(note.replace(/[ ]*\n/g, '<br>\n'))
-                  const html = (
-                    <div className="note" key={'note-' + index}>
-                      <span className="note-label">{split[0]}:</span>
-                      <span dangerouslySetInnerHTML={self.allowHTML(note)} />
-                    </div>
-                  )
-                  notesHtml.push(html)
-                } else {
-                  notesHtml = originalFn.call(self)
-                }
-              }
+              notesHtml = originalFn.call(self)
             }
           }
         })
